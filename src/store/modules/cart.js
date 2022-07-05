239614cac5afcf803c4fbbdb5333362fbcc4852b
p.a.toribio <p.a.toribio@viseven.com>
2022-07-05 18:56:29 -0300

    introduje metodo reduce para calcular precio del carrito

--- src/store/modules/cart.js
+++ src/store/modules/cart.js
@@ -14,33 +14,57 @@ export default {
   mutations: {
     ADD_TO_CART: (state, { product, quantity }) => {
       if (quantity == 0) return;
-      const isInCart = state.cart.products.find((item) => item.id === product.id);
+      const isInCart = state.cart.products.find(
+        (item) => item.id === product.id
+      );
       if (isInCart) {
+        console.log(product);
         product.quantity += quantity;
-        state.cart.totalCartPrice += product.price * quantity;
+        state.cart.totalCartPrice = state.cart.products.reduce(
+          (acc, product) => {
+            return acc + product.price * product.quantity;
+          },
+          0
+        );
         localStorage.setItem("Cart", JSON.stringify(state.cart));
       } else {
+        console.log(product);
+
         product.quantity = 0;
         product.quantity += quantity;
         state.cart.products.push(product);
-        state.cart.totalCartPrice += product.price * quantity;
+        state.cart.totalCartPrice = state.cart.products.reduce(
+          (acc, product) => {
+            return acc + product.price * product.quantity;
+          },
+          0
+        );
         localStorage.setItem("Cart", JSON.stringify(state.cart));
       }
     },
-    REMOVE_FROM_CART: (state, {product, quantity}) => {
+    REMOVE_FROM_CART: (state, { product, quantity }) => {
       if (quantity == 1) {
         product.quantity -= 1;
-        state.cart.totalCartPrice -= product.price;
+        state.cart.totalCartPrice = state.cart.products.reduce(
+          (acc, product) => {
+            return acc + product.price * product.quantity;
+          },
+          0
+        );
         localStorage.setItem("Cart", JSON.stringify(state.cart));
         if (product.quantity == 0) {
-          let filteredCart = state.cart.products.filter((item) => item.id != product.id);
+          let filteredCart = state.cart.products.filter(
+            (item) => item.id != product.id
+          );
           state.cart.products = filteredCart;
           localStorage.setItem("Cart", JSON.stringify(state.cart));
         }
       } else if (quantity == "all") {
-        let filteredCart = state.cart.products.filter((item) => item.id != product.id);
+        let filteredCart = state.cart.products.filter(
+          (item) => item.id != product.id
+        );
         state.cart.products = filteredCart;
-        state.cart.totalCartPrice -= product.price * product.quantity
+        state.cart.totalCartPrice -= product.price * product.quantity;
         localStorage.setItem("Cart", JSON.stringify(state.cart));
       }
     },
